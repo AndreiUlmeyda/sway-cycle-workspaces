de63793e5aa0901db5bbbc220189e8878ba8ce51
Adrian Schurz <adrian.schurz@check24.de>
2021-04-30 21:56:49 +0200

    Parse the input

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,29 @@
 module Main where
 
-import Lib (someFunc)
+import Lib (cycleWorkSpaces)
+import System.Environment (getArgs)
 
 main :: IO ()
-main = someFunc
+main = getArgs >>= interact . cycleWorkspacesWithMode . modeFromArgs
+
+data Mode = Up | Down
+
+data Workspace = Workspace
+  { output :: String,
+    workspaceIndex :: Int,
+    focused :: Bool
+  }
+  deriving (Show)
+
+cycleWorkspacesWithMode :: Mode -> String -> String
+cycleWorkspacesWithMode _ input = show $ map (parseWorkspace . words) (lines input)
+
+parseWorkspace :: [String] -> Workspace
+parseWorkspace input = Workspace {output = output, workspaceIndex = workspaceIndex, focused = focused}
+  where
+    output = head input
+    workspaceIndex = read (input !! 1) :: Int
+    focused = (input !! 2) == "true"
+
+modeFromArgs :: a -> Mode
+modeFromArgs = const Up
