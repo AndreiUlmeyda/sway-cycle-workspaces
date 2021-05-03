bcc13016c005ffa7cb99eef221830df779c10f3d
Adrian Schurz <adrian.schurz@check24.de>
2021-05-03 16:09:19 +0200

    Add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import Lib (next)
+import Test.Hspec
+import Types (Workspace (..))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "next" $ do
+    it "given a single workspace it should return its index" $ do
+      let workspaceOne = Workspace {output = "output1", workspaceIndex = 1, focused = True}
+       in next [workspaceOne] `shouldBe` 1
+    it "given a two workspaces with the first focused it should return the second index" $ do
+      let workspaceOne = Workspace {output = "output1", workspaceIndex = 1, focused = True}
+      let workspaceTwo = Workspace {output = "output1", workspaceIndex = 2, focused = True}
+       in next [workspaceOne, workspaceTwo] `shouldBe` 2
