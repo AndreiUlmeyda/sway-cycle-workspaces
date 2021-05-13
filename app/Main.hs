08eee7c7cfd9161f439d1711c52662c009de252d
Adrian Schurz <adrian.schurz@check24.de>
2021-05-13 21:10:17 +0200

    Check for empty inputs

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,21 @@ module Main where
 
 import Control.Monad (void)
 import Lib (nextWorkspace)
-import Mode (modeFromArgs)
+import Mode (Mode, modeFromArgs)
 import System.Environment (getArgs)
 import System.Process (readProcess)
+import Types (InputLine)
 
 main :: IO ()
 main = do
   mode <- fmap modeFromArgs getArgs
   input <- fmap lines getContents
-  void $ readProcess "swaymsg" ["workspace", "number", nextWorkspace mode input] swayMsgStdIn
+  changeWorkspaceUpOrDown mode input
+
+changeWorkspaceUpOrDown :: Mode -> [InputLine] -> IO ()
+changeWorkspaceUpOrDown mode input
+  | all null input = print "Error: A list of workspaces needs to be provided as input."
+  | otherwise = void $ readProcess "swaymsg" ["workspace", "number", nextWorkspace mode input] swayMsgStdIn
 
 -- the swaymsg command is determined by its arguments, nothing is piped in
 swayMsgStdIn :: String
