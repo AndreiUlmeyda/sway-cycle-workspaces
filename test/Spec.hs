3153a11ba92da7e40f4b735226e2039a0df4d648
Adrian Schurz <adrian.schurz@check24.de>
2021-05-13 21:11:09 +0200

    WIP redo unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,16 @@
-import Lib (next)
-import Test.Hspec
+import Lib (nextWorkspace)
+import Mode (Mode (Next, Previous))
+import Test.Hspec (describe, hspec, it, shouldBe)
 import Types (Workspace (..))
 
 main :: IO ()
 main = hspec $ do
-  describe "next" $ do
-    it "given a single workspace it should return its index" $ do
-      let workspaceOne = Workspace {output = "output1", workspaceIndex = 1, focused = True}
-       in next [workspaceOne] `shouldBe` 1
-    it "given a two workspaces with the first focused it should return the second index" $ do
-      let workspaceOne = Workspace {output = "output1", workspaceIndex = 1, focused = True}
-      let workspaceTwo = Workspace {output = "output1", workspaceIndex = 2, focused = True}
-       in next [workspaceOne, workspaceTwo] `shouldBe` 2
+  describe "nextWorkspace" $ do
+    it "given on input it should return its index" $ do
+      let workspaceDesciption = ["output1 1 true"] -- Workspace {output = "output1", workspaceIndex = 1, focused = True}
+       in nextWorkspace Previous workspaceDesciption `shouldBe` "1"
+
+-- it "given a two workspaces with the first focused it should return the second index" $ do
+--   let workspaceOne = Workspace {output = "output1", workspaceIndex = 1, focused = True}
+--   let workspaceTwo = Workspace {output = "output1", workspaceIndex = 2, focused = True}
+--    in next [workspaceOne, workspaceTwo] `shouldBe` 2
