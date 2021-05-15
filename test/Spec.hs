2ddca1ebd6c57be4344503590e5a7668c3dc1f59
Adrian Schurz <adrian.schurz@check24.de>
2021-05-15 18:18:46 +0200

    More error case unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,5 @@
 import Data.Either ()
-import Errors (ErrorMessage, errorNoNextWorkspace, errorTooFewInputWorkspaces, errorWrongInputLayout)
+import Errors (ErrorMessage, errorNoNextWorkspace, errorNoPreviousWorkspace, errorNotExactlyOneFocusedWorkspace, errorTooFewInputWorkspaces, errorWrongInputLayout)
 import Lib (WorkspaceIndex (WorkspaceIndex), changeWorkspace)
 import Mode (Mode (Next, Previous))
 import Test.Hspec (describe, hspec, it, shouldBe)
@@ -14,9 +14,18 @@ main = hspec $ do
     it "given a single workspace should return an error indicating insufficient input" $ do
       let workspaceDesciption = ["output 1 true"]
        in changeWorkspace Previous workspaceDesciption `shouldBe` Left errorTooFewInputWorkspaces
-    it "given focus on the first workspace and Next-Mode should return the second" $ do
+    it "given focus on the first workspace and mode 'Next' should return the second" $ do
       let workspaceDesciption = ["output 1 true", "output 2 false"]
        in changeWorkspace Next workspaceDesciption `shouldBe` Right (WorkspaceIndex "2")
-    it "given focus on the last workspace and Next-Mode should return an error indicating that there is no next workspace" $ do
+    it "given focus on the last workspace and mode 'Next' should return an error indicating that there is no next workspace" $ do
       let workspaceDesciption = ["output 1 false", "output 2 true"]
        in changeWorkspace Next workspaceDesciption `shouldBe` Left errorNoNextWorkspace
+    it "given focus on the first workspace and mode 'Previous' should return an error indicating that there is no previous workspace" $ do
+      let workspaceDesciption = ["output 1 true", "output 2 false"]
+       in changeWorkspace Previous workspaceDesciption `shouldBe` Left errorNoPreviousWorkspace
+    it "given no focused workspace should return an error indicating a single focused workspace is expected" $ do
+      let workspaceDesciption = ["output 1 false", "output 2 false"]
+       in changeWorkspace Previous workspaceDesciption `shouldBe` Left errorNotExactlyOneFocusedWorkspace
+    it "given two focused workspaces should return an error indicating a single focused workspace is expected" $ do
+      let workspaceDesciption = ["output 1 true", "output 2 true"]
+       in changeWorkspace Previous workspaceDesciption `shouldBe` Left errorNotExactlyOneFocusedWorkspace
