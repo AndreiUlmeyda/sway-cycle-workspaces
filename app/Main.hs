b08bae04abd42e7942d345e701d123d14bf21750
Adrian Schurz <adrian.schurz@check24.de>
2021-08-10 23:29:12 +0200

    WIP call swaymsg and jq using turtle

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Errors (ErrorMessage (ErrorMessage))
@@ -5,12 +7,36 @@ import InputValidation (parseInput)
 import Mode (parseArgumentsAndProvideHelpText)
 import NewWorkspace (changeWorkspace)
 import Types (WorkspaceIndex (WorkspaceIndex))
+import Turtle (textToLines, Shell, Line, ExitCode, Text, textToLine, view, shellStrict, empty, unsafeTextToLine)
+import Data.Maybe (fromJust, Maybe)
+import Data.Text (replace)
 
 main :: IO ()
-main = do
-  mode <- parseArgumentsAndProvideHelpText
-  input <- fmap (parseInput mode) getContents
-  printResultOrAbort (changeWorkspace mode input)
+main = view (preformat =<< getWorkspaceDescription)
+
+--workspaceDescriptionJson :: Shell Line
+--workspaceDescriptionJson = fromJust . textToLine <$> fmap snd getWorkspaceDescription
+
+getWorkspaceDescription :: Shell (ExitCode, Text)
+getWorkspaceDescription = shellStrict "swaymsg --raw --type get_workspaces" empty
+
+preformat :: (ExitCode, Text) -> Shell (ExitCode, Text)
+preformat input = shellStrict "./json-to-workspace-lines.jq" (workspaceDescriptionFrom input)
+
+workspaceDescriptionFrom :: (ExitCode, Text) -> Shell Line
+workspaceDescriptionFrom= pure . textToLine' . (replace "\n" "") .snd
+
+textToLine' :: Text -> Line
+textToLine' input
+  | Nothing <- input' = unsafeTextToLine "derp"
+  | Just line <- input' = line
+  where input' = textToLine input
+
+--  |Text
+--main = do
+--  mode <- parseArgumentsAndProvideHelpText
+--  input <- fmap (parseInput mode) getContents
+--  printResultOrAbort (changeWorkspace mode input)
 
 printResultOrAbort :: Either ErrorMessage WorkspaceIndex -> IO ()
 printResultOrAbort (Left (ErrorMessage errorMessage)) = error errorMessage
